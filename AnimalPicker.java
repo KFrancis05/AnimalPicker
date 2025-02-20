204431f84eb5930c50d7217416ea9c03d6f80d56
KFrancis05 <jgf0803@gmail.com>
2025-02-19 19:41:11 -0500

    Added Scanner, option for dog or cat, and placeholder text based on answer

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
 
 public class AnimalPicker {
+    Scanner s = new Scanner(System.in);
     
+    System.out.println("Which animal would you like to see? Dog or Cat?");
+    String answer = s.nextLine();
+    s.nextLine();
+
+    if((answer.toLowerCase()).equals("dog")){
+        System.out.println("DOG ASCII ART HERE");
+    }
+    else if ((answer.toLowerCase()).equals("cat")){
+        System.out.println("CAT ASCII ART HERE");
+    }
 }
